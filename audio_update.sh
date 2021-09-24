566640d23ed22af7c165a879cdbb28dfd5058bff
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2021-09-24 17:43:31 +0100

    Update audio_update.sh

--- audio_update.sh
+++ audio_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo “blacklist snd_bcm2835” > /etc/modprobe.d/raspbi-blacklist.conf
+echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
 sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
 
