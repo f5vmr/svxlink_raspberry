9f97998b10d9fb1261961dcd713375c1b0fc5651
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-08 16:36:05 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ ! -f $FLAG ]; then
 	echo `date` *** commence build ***
 # Installing other packages
 	echo `date` Installing required software packages …
-	sudo apt install g++ make cmake libsigc++-2.0-dev php8.0 libgsm1-dev libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y
+	sudo apt install g++ make cmake libsigc++-2.0-dev php8.0 libgsm1-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y
 
 	echo -n “Enter the node callsign: “
 	read CallVar
