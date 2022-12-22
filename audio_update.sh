6a61b9ac963252f57c540a0c387f3188b1240246
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-12-22 17:07:57 +0000

    Updated to include Modified CM-108 if present

--- audio_update.sh
+++ audio_update.sh
@@ -2,3 +2,22 @@
 echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
 sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
 
+while true; do
+	echo -e "Do you have a modified CM-108 USB Card?}"
+	read -p "Yes I have (y/N) " yn
+	case $yn in
+		[Yy] ) echo ok, Let's add the updated rules - ; 
+		sudo cp cm-108.rules /etc/udev/rules.d/
+		sudo udevadm control --reload-rules
+		sudo udevadm trigger
+    CM=1
+		break
+	esac
+done
+break
+
+if $CM=1 then echo "Modified CM-108 soundcard present"
+fi
+echo "Audio Updates complete."
+
+
