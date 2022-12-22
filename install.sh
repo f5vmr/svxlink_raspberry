70cc18191bb8b4412ab7edae011d4fbb00285d70
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-12-22 18:50:30 +0000

    Added Crontab and script for maintaining log

--- install.sh
+++ install.sh
@@ -15,8 +15,9 @@ if [ ! -f $FLAG ]; then
 # Installing other packages
 	echo `date` Installing required software packages …
 	sudo apt install g++ make cmake libsigc++-2.0-dev php8.0 libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y
-        
+	echo         
 	echo -n “Enter the node callsign: \n“
+	echo
 	
 	read CallVar
 	if [ “$CallVar” == “” ]; then
@@ -24,8 +25,10 @@ if [ ! -f $FLAG ]; then
 		exit 1
 	fi
 	CALL=${CallVar^^}
+	echo
 	echo `date` Creating Node $CALL
 # Creating Groups and Users
+	echo
 	echo `date` Creating Groups and Users 
 	sudo groupadd svxlink
 	sudo useradd -g svxlink -d /etc/svxlink svxlink
@@ -39,18 +42,7 @@ if [ ! -f $FLAG ]; then
 	sudo git clone https://github.com/sm0svx/svxlink.git
 	sudo mkdir svxlink/src/build
 	fi
-# Updating from repo and building software
-#	echo `date` Updating from repo and building software …
-#       cd
-#	cd svxlink
-#	VERSION=`sudo grep “SVXLINK=“ $VERSIONS | awk -F= '{print $2}'`
-#	echo `date` Current Version: $VERSION
-#	#
-#	sudo git pull
-#		if [ “$?” != 0 ]; then
-#			echo `date` git pull request failed, aborting!
-#			exit 1
-#	fi
+	
 	NEWVERSION=`sudo grep “SVXLINK=“ $VERSIONS | awk -F= '{print $2}'`
 	echo `date` New Version: $NEWVERSION
 # Compilation
@@ -65,23 +57,25 @@ if [ ! -f $FLAG ]; then
 	sudo mkdir local
 	sudo cp * local
 	sudo ldconfig
-# if [ ! -F $FLAG ]; then
-	# Installing United Kingdom Sound files
+# Installing United Kingdom Sound files
 	cd /usr/share/svxlink/sounds
 	sudo wget https://g4nab.co.uk/wp-content/uploads/2022/12/en_GB.tar.gz
 	sudo tar -zxvf en_GB.tar.gz
 	sudo rm en_GB.tar.gz
 #.      for the French Language use sudo wget https://g4nab.co.uk/wp-content/uploads/2021/07/fr_FR.tar.gz and change the 2 lines below.      
 #.      pour la langue française uilise cela dessus ^ et donc changé les deux lignes qui la suivent.
-echo `date` backing up configuration to : $CONF.bak
+	echo `date` backing up configuration to : $CONF.bak
 	cd $OP
 	sudo cp -p $CONF $CONF.bak
 #
 	cd $HOME
 	echo `date` Downloading prepared configuration files from G4NAB …
+	sudo mkdir scripts
 	sudo cp -r svxlink_raspberry/svxlink.conf $OP
 	sudo cp -r svxlink_raspberry/gpio.conf $OP
 	sudo cp -r svxlink_raspberry/node_json.conf $OP/node_info.json
+	sudo cp -r svxlink_raspberry/resetlog.sh ../scripts/
+	(crontab -l 2>/dev/null; echo "59 23 * * * /home/pi scripts/resetlog.sh ") | crontab -
 #
 	echo `date` Setting Callsign to $CALL
 	sudo sed -i "s/MYCALL/$CALL/g" $CONF
@@ -94,8 +88,8 @@ echo `date` backing up configuration to : $CONF.bak
 	sudo sed -i "s/PEAK_METER=1/PEAK_METER=0/g" $CONF
 #
 	echo `date` Updating SplashScreen on startup
-#	sudo sed -i "s/MYCALL/$CALL/g" /etc/update-motd.d/10-uname
-#	sudo chmod 0755 /etc/update-motd.d/10-uname
+	sudo sed -i "s/MYCALL/$CALL/g" /etc/update-motd.d/10-uname
+	sudo chmod 0755 /etc/update-motd.d/10-uname
 #	
 	echo `date` enabling GPIO setup service and svxlink service …
 	sudo systemctl enable svxlink_gpio_setup
