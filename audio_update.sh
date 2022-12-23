f737ad9bb75d5688715a526a6a9e171191dac162
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-12-23 11:09:12 +0000

    Update audio_update.sh

--- audio_update.sh
+++ audio_update.sh
@@ -6,13 +6,15 @@ while true; do
 	echo -e "Do you have a modified CM-108 USB Card?"
 	read -p "Yes I have (y/N) " yn
 	case $yn in
-		[Yy] ) echo "ok, Let's add the updated rules - ;" 
+		[Yy] ) echo "ok, Let's add the updated rules - " 
 		sudo cp cm-108.rules /etc/udev/rules.d/
 		sudo udevadm control --reload-rules
 		sudo udevadm trigger
+		;;
     		CM=true
 		break;
 	esac
+	break;
 done
 
 
