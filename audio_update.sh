1e39ac3a588b3e97618346d06f00863dbbe2839f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-12-23 11:02:34 +0000

    Update audio_update.sh

--- audio_update.sh
+++ audio_update.sh
@@ -3,10 +3,10 @@ echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
 sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
 
 while true; do
-	echo -e "Do you have a modified CM-108 USB Card?}"
+	echo -e "Do you have a modified CM-108 USB Card?"
 	read -p "Yes I have (y/N) " yn
 	case $yn in
-		[Yy] ) echo ok, Let's add the updated rules - ; 
+		[Yy] ) echo "ok, Let's add the updated rules - ;" 
 		sudo cp cm-108.rules /etc/udev/rules.d/
 		sudo udevadm control --reload-rules
 		sudo udevadm trigger
@@ -14,7 +14,7 @@ while true; do
 		break;
 	esac
 done
-break;
+
 
 		if $CM=true 
 		then 
