15a0d10e37b2c865b3bbcc2f1455ead12c88d255
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-12-23 13:37:25 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -99,7 +99,7 @@ VERSIONS=svxlink/src/versions
 	then
 	sudo sed -i "/PTT_TYPE/ i "HID_DEVICE=/dev/hidraw0"' $CONF
 	sudo sed -i "s/PTT_TYPE=GPIO/PTT_TYPE=Hidraw/g" $CONF
-	sudo sed -i "s/PTT_PORT=GPIO/PTT_PORT=/dev/hidraw0/g" $CONF
+	sudo sed -i "s/PTT_PORT=GPIO/PTT_PORT=\/dev\/hidraw0/g" $CONF
 	sudo sed -i "s/PTT_PIN=gpio24/HID_PTT_PIN=GPIO3/g" $CONF
 	sudo sed -i "s/\#MUTE/MUTE/g" /etc/svxlink/svxlink.d/ModuleEchoLink.conf
 	sudo sed -i "s/\#DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleEchoLink.conf
