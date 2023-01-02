6fcd18704bdf8df6ac9ff6c8aaae3821e468d1cc
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-02 14:40:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,7 +81,7 @@ VERSIONS=svxlink/src/versions
 	sudo cp -r svxlink_raspberry/svxlink.conf $OP
 	sudo cp -r svxlink_raspberry/gpio.conf $OP
 	sudo cp -r svxlink_raspberry/node_info.json $OP/node_info.json
-	sudo cp -r svxlink_raspberry/resetlog.sh ../scripts/resetlog.sh
+	sudo cp -r svxlink_raspberry/resetlog.sh ~/scripts/resetlog.sh
 	(crontab -l 2>/dev/null; echo "59 23 * * * /home/pi scripts/resetlog.sh ") | crontab -
 #
 	echo `date` Setting Callsign to $CALL
