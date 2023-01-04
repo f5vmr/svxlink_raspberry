8565ed54aca40994d1264a51565a20660ed622bf
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-04 12:51:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -109,13 +109,9 @@ VERSIONS=svxlink/src/versions
 	sudo sed -i "s/\#MUTE/MUTE/g" /etc/svxlink/svxlink.d/ModuleEchoLink.conf
 	sudo sed -i "s/\#DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleEchoLink.conf
 	sudo sed -i "s/\#MUTE/MUTE/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-	sudo sed -i "s/\#DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-	
-
-
+	sudo sed -i "s/\#DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf	
 
 	fi
-#
 	echo `date` "${RED} Authorise GPIO setup service and svxlink service${NORMAL}"
 	sudo systemctl enable svxlink_gpio_setup
 	sleep 10
