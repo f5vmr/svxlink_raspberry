f6d9a276178de0f5fb7762d4ffdf260b001a7429
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-04-29 00:10:04 +0100

    Changes made
    Removed audio out from hdmi that was preventing usb sound card appearing as card 0 every time.

--- audio_update.sh
+++ audio_update.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
 sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
+sudo sed -i "s/dtoverlay=vc4-kms-vd3/dtoverlay=vc4-kms-vd3,noaudio/g /boot/config.txt
 
 while true; do
 	echo -e "Do you have a modified CM-108 USB Card?"
-	read -p "Yes I have (y/N) " yn
+	read -p "(y/n)" yn
 	case $yn in
-		[Yy] ) echo "ok, Let's add the updated rules - " 
-		sudo cp svxlink_raspberry/cm-108.rules /etc/udev/rules.d/
-		sudo udevadm control --reload-rules
-		sudo udevadm trigger
-    		card=true
-		break
-		;;
-	* ) break 
-		;;
+		[Yy] ) 
+		echo "ok, Let's add the updated rules - ";
+		sudo cp svxlink_raspberry/cm-108.rules /etc/udev/rules.d/;
+		sudo udevadm control --reload-rules;
+		sudo udevadm trigger;
+    		card=true;
+		break;;
+		[Nn] )
+		echo "ok, then I will make no other changes";;
+		card=false;
+		exit;;
+	        * ) echo invalid response;;
 	esac
 	
 done
