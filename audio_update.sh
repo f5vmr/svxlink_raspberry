cf91538848b1e9005260a9b748ace491ea20e893
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-04-30 20:32:01 +0100

    Update audio_update.sh
    changes to bash script

--- audio_update.sh
+++ audio_update.sh
@@ -4,21 +4,20 @@ sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
 sudo sed -i "s/dtoverlay=vc4-kms-vd3/dtoverlay=vc4-kms-vd3,noaudio/g /boot/config.txt
 
 while true; do
-	echo -e "Do you have a modified CM-108 USB Card?"
-	read -p "(y/n)" yn
+	read -p "Do you have a modified CM-108 USB Card? (Y/N) " yn
 	case $yn in
-		[Yy] ) 
-		echo "ok, Let's add the updated rules - ";
+		[Yy]* ) 
+		echo "Ok, Let's add the updated rules - ";
 		sudo cp svxlink_raspberry/cm-108.rules /etc/udev/rules.d/;
 		sudo udevadm control --reload-rules;
 		sudo udevadm trigger;
     		card=true;
 		break;;
-		[Nn] )
-		echo "ok, then I will make no other changes";;
+		[Nn]* )
+		echo "ok, then I will make no other changes";
 		card=false;
-		exit;;
-	        * ) echo invalid response;;
+		break;;
+	        * ) echo "Please answer Y or N.";;
 	esac
 	
 done
