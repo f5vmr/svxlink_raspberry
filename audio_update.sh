53d18d51d8dc0e2e390c56f4c467f9e0dec7c225
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-05-01 11:39:11 +0100

    File replaced with working model

--- audio_update.sh
+++ audio_update.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
 sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
-sudo sed -i "s/dtoverlay=vc4-kms-vd3/dtoverlay=vc4-kms-vd3,noaudio/g /boot/config.txt
+sudo sed -i "s/dtoverlay=vc4-kms-vd3/dtoverlay=vc4-kms-vd3,noaudio/g" /boot/config.txt
 
 while true; do
-	read -p "Do you have a modified CM-108 USB Card? (Y/N) " yn
-	case $yn in
-		[Yy]* ) 
-		echo "Ok, Let's add the updated rules - ";
-		sudo cp svxlink_raspberry/cm-108.rules /etc/udev/rules.d/;
-		sudo udevadm control --reload-rules;
-		sudo udevadm trigger;
-    		card=true;
-		break;;
-		[Nn]* )
-		echo "ok, then I will make no other changes";
-		card=false;
-		break;;
-	        * ) echo "Please answer Y or N.";;
-	esac
-	
+        echo "Do you have a modified CM-108 USB Card? - Y/N "
+        read yn
+        case $yn in
+                [Yy]* )
+                echo "Ok, Let's add the updated rules"
+                sudo cp svxlink_raspberry/cm-108.rules /etc/udev/rules.d/
+                sudo udevadm control --reload-rules
+                sudo udevadm trigger
+                card=true
+                break
+                ;;
+                [Nn]* )
+                echo "ok, then I will make no other changes"
+                card=false
+                break
+                ;;
+                * ) echo "Please answer Y or N."
+                ;;
+        esac
+        
 done
 
 
-		if [ "$card"=true ] ;
-		then 
-		echo "Modified CM-108 soundcard present"
-		fi
-		
-		echo "Audio Updates complete."
+                if [ $card=true ] ;
+                then 
+                echo "Modified CM-108 soundcard present"
+                else
+                echo "Normal USB soundcard present"
+                fi
+                
+                echo "Audio Updates complete."
 
 
