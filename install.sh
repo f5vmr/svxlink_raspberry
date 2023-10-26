72f4002c41946d9ee3e587154ffc55b6a50c5745
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-10-26 15:38:27 +0100

    Update nodejs

--- install.sh
+++ install.sh
@@ -15,7 +15,13 @@ GPIO=/etc/svxlink/gpio.conf
 HOME=/home/pi
 OP=/etc/svxlink
 cd
-sudo curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
 sudo apt update
 sudo apt upgrade -y
 VERSIONS=svxlink/src/versions
@@ -24,7 +30,7 @@ VERSIONS=svxlink/src/versions
 
 # Installing other packages
 	echo -e `date` " ${YELLOW} Installing required software packages${NORMAL}"
-	sudo apt install build-essential g++ make cmake libsigc++-2.0-dev php8.0 libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y
+	sudo apt install build-essential g++ make cmake libsigc++-2.0-dev php8.0 nodejs libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y
 	echo         
 	echo -e "${GREEN} Enter the node callsign: \n ${NORMAL}"
 	echo
