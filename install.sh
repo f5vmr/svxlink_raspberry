848f39177098ba3a5177365025af69445d3a6028
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-03-20 07:58:13 +0000

    Update install.sh
    Changed user 'pi' to $USER so create of relative file-path is automatic

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ YELLOW="\033[1;33m"
 echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
 sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
 sudo sed -i "s/dtoverlay=vc4-kms-v3d/dtoverlay=vc4-kms-v3d,noaudio/g" /boot/config.txt
-sudo cp /home/pi/svxlink_raspberry/asound.conf /etc/modprobe.d/asound.conf
+sudo cp /home/$USER/svxlink_raspberry/asound.conf /etc/modprobe.d/asound.conf
 echo snd-aloop > /etc/modules
-sudo cp /home/pi/svxlink_raspberry/loopback.conf /etc/asound.conf
+sudo cp /home/$USER/svxlink_raspberry/loopback.conf /etc/asound.conf
 while true; do
         echo "Do you have a modified CM-108 USB Card? - Y/N "
         read yn
@@ -126,12 +126,12 @@ VERSIONS=svxlink/src/versions
 #
 	cd $HOME
 	echo -e `date` "${RED} Downloading prepared configuration files from G4NAB …${NORMAL}"
-	sudo mkdir /home/pi/scripts
+	sudo mkdir /home/$USER/scripts
 	sudo cp -f svxlink_raspberry/svxlink.conf $OP
 #	sudo cp -f svxlink_raspberry/gpio.conf $OP
 #	sudo cp -f svxlink_raspberry/node_info.json $OP/node_info.json
 	sudo cp -f svxlink_raspberry/resetlog.sh scripts/resetlog.sh
-	(crontab -l 2>/dev/null; echo "59 23 * * * /bin/bash /home/pi/scripts/resetlog.sh ") | crontab -
+	(crontab -l 2>/dev/null; echo "59 23 * * * /bin/bash /home/$USER/scripts/resetlog.sh ") | crontab -
 #
 	echo `date` Setting Callsign to $CALL
 	sudo sed -i "s/MYCALL/$CALL/g" $CONF
