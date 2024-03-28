b99ae5ca6ed66521f2dba1537ba44dd2dc3c3aff
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-03-28 14:50:41 +0000

    m

--- functions/language.sh
+++ functions/language.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 ######## WHICH LANGUAGE ########
 ## options en_GB or fr_FR ##
-function which_language {
-OPTION=$(whiptail --title "$TITLE" --menu "Select Default Language" 14 78 3 \
-    "1" "English - en_GB" \
-    "2" "French - fr_FR" 3>&1 1>&2 2>&3)
+function which_language() {
+    OPTION=$(whiptail --title "$TITLE" --menu "Select Default Language" 14 78 3 \
+        "1" "English - en_GB" \
+        "2" "French - fr_FR" 3>&1 1>&2 2>&3)
+
     case $OPTION in
-    1) ENGLISH_install=true;;
-    2) ENGLISH_install=false;;
+        1) ENGLISH_install=true;;
+        2) ENGLISH_install=false;;
+        *)
+            # Default action if no valid option is selected
+            echo "No valid option selected."
+            ;;
     esac
-echo "Default Language: $OPTION"
-    else
-    exit
-    fi
+
+    echo "Default Language: $OPTION"
 }
+
