a96a79666753a3c2811f8e8fae0d063c4189a883
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-03-30 20:50:39 +0000

    m

--- functions/update.sh
+++ functions/update.sh
@@ -23,6 +23,6 @@ whiptail --title "Updating" --msgbox "Uprating repositories for nodejs and futur
 whiptail --title "Build Essentials" --msgbox "Adding all the packages necessary for Svxlink. Hit OK to continue" 8 78
 
 	echo -e `date` " ${YELLOW} ****Installing required software packages ****${NORMAL}" >> /var/log/install.log
-#	sudo apt install build-essential g++ make cmake libsigc++-2.0-dev php8.2 nodejs libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial sqlite3 toilet -y
+#	sudo apt install build-essential g++ make cmake libsigc++-2.0-dev php8.2 nodejs libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial sqlite3 php8.2-sqlite3 toilet -y
 
 }
