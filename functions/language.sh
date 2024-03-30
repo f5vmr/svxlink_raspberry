a1be13d8de0dd5504e03e43141a103590e50ffe4
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-03-30 15:04:50 +0000

    m

--- functions/language.sh
+++ functions/language.sh
@@ -6,15 +6,22 @@ function which_language() {
         "1" "English - en_GB" \
         "2" "French - fr_FR" 3>&1 1>&2 2>&3)
 
-    case $OPTION in
-        1) ENGLISH_install=true;;
-        2) ENGLISH_install=false;;
-        *)
-            # Default action if no valid option is selected
-            echo "No valid option selected."
-            ;;
-    esac
-
-    echo "Default Language: $OPTION" 
+    if [ "$OPTION" -eq 1 ]; then
+            sudo sed -i 's/^# en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen
+            sudo locale-gen
+            sudo update-locale LANG=en_GB.UTF-8
+    elif [ "$OPTION" -eq 2 ]; then
+            sudo sed -i 's/^# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
+            sudo locale-gen
+            sudo update-locale LANG=fr_FR.UTF-8
+    else
+        # Default action if no valid option is selected
+        echo "No valid option selected."
+    fi
 }
+
+
+ 
+
+
 
