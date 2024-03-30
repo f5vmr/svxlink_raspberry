46441124ef99beb6ef24933ee6f398014f48bb06
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-03-30 16:33:18 +0000

    m

--- functions/check_os.sh
+++ functions/check_os.sh
@@ -4,14 +4,14 @@ function check_os {
 codename = $(lsb_release -c | grep -oP '(?<=Codename:\s)\w+')
 
 # Print the value of the variable
-if [ $codename != "bookworm" ] then
+if [ $codename != "bookworm" ]; then
 whiptail --title "SVXLink" --msgbox "This script is only for the Bookworm Linux distribution.\n\n Shutdown and Change to OS Bookworm, with user pi." 8 78
 fi
 # Assign the current user of the terminal to a variable
 current_user=$(whoami)
 
 # Print the value of the variable
-if [ $current_user != "pi" ] then
+if [ $current_user != "pi" ]; then
 whiptail --title "SVXLink" --msgbox "This script is only for the pi user. Shutdown and reload the OS with user pi" 8 78
 fi
 }
