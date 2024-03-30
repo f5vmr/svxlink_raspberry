2a9c8de2e6c31c1be105e097166cda546418815b
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-03-30 16:38:56 +0000

    m

--- functions/check_os.sh
+++ functions/check_os.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Assign the codename from the output of lsb_release -c to a variable
 function check_os {
-codename = $(lsb_release -c | grep -oP '(?<=Codename:\s)\w+')
+ 
+OS_name=$(lsb_release -c | grep -oP '(?<=Codename:\s)\w+')
 
 # Print the value of the variable
-if [ $codename != "bookworm" ]; then
+if [$OS_name != "bookworm"]; then
 whiptail --title "SVXLink" --msgbox "This script is only for the Bookworm Linux distribution.\n\n Shutdown and Change to OS Bookworm, with user pi." 8 78
 fi
 # Assign the current user of the terminal to a variable
