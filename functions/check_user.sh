37a785e441e15dd34406d275d90aa86a013df56d
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-01 15:13:39 +0100

    m

--- functions/check_user.sh
+++ functions/check_user.sh
@@ -3,7 +3,7 @@
 usercheck() {
     # Get the current user
     local current_user
-    current_user=$(get_current_user)
+    current_user=$(get_current_user) >> /var/log/install.log 2>&1
 
     # Check if the current user is 'pi'
     if [ "$current_user" != "pi" ]; then
