61ab458bd9e410bc4dcb3bb97f3e0fba929325f3
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-01 14:59:16 +0100

    m

--- functions/check_os.sh
+++ functions/check_os.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 # Assign the codename from the output of lsb_release -c to a variable
+#function check_os {
+# 
+#OS_name=$(lsb_release -c | grep -oP '(?<=Codename:\s)\w+')#
+
+## Print the value of the variable
+#if [ $OS_name != 'bookworm' ]; then
+#whiptail --title "SVXLink" --msgbox "This script is only for the Bookworm Linux distribution.\n\n Shutdown and Change to OS Bookworm, with user pi." 8 78
+#fi
+## Assign the current user of the terminal to a variable
+#source ./check_user.sh
+#user=$(get_current_user)
+#current_user=$user#
+
+## Print the value of the variable
+#if [ $current_user != "pi" ]; then
+#whiptail --title "SVXLink" --msgbox "This script is only for the pi user. Shutdown and reload the OS with user pi" 8 78
+#fi
+# 
+#}
 function check_os {
- 
-OS_name=$(lsb_release -c | grep -oP '(?<=Codename:\s)\w+')
+    # Retrieve the OS codename
+    OS_name=$(lsb_release -c | grep -oP '(?<=Codename:\s)\w+')
 
-# Print the value of the variable
-if [ $OS_name != 'bookworm' ]; then
-whiptail --title "SVXLink" --msgbox "This script is only for the Bookworm Linux distribution.\n\n Shutdown and Change to OS Bookworm, with user pi." 8 78
-fi
-# Assign the current user of the terminal to a variable
-source ./check_user.sh
-user=$(get_current_user)
-current_user=$user
+    # Check if the OS is Bookworm
+    if [ "$OS_name" != 'bookworm' ]; then
+        whiptail --title "SVXLink" --msgbox "This script is only for the Bookworm Linux distribution.\n\n Shutdown and Change to OS Bookworm, with user pi." 8 78
+        exit 1  # Exit the script if the OS is not Bookworm
+    fi
 
-# Print the value of the variable
-if [ $current_user != "pi" ]; then
-whiptail --title "SVXLink" --msgbox "This script is only for the pi user. Shutdown and reload the OS with user pi" 8 78
-fi
- 
+    # Check if the current user is 'pi'
+    source ./check_user.sh
+    current_user=$(get_current_user)
+
+    if [ "$current_user" != "pi" ]; then
+        whiptail --title "SVXLink" --msgbox "This script is only for the pi user. Shutdown and reload the OS with user pi" 8 78
+        exit 1  # Exit the script if the current user is not 'pi'
+    fi
 }
