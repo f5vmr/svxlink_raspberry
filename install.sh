b2916de94477089b1db34b74066e677573f31094
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-01 17:57:10 +0100

    m

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ OP=/etc/svxlink
 ########## BUILD ##########
 whiptail --title "Updating" --msgbox "Uprating repositories for nodejs and future expansion. Hit OK to continue" 8 78
 
-	sudo echo -e $(date) " ${YELLOW}  *** updates and upgrades *** ${NORMAL}" >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) " ${YELLOW}  *** updates and upgrades *** ${NORMAL}" >> /var/log/install.log'
 	sudo apt-get update
 	sudo apt-get install -y ca-certificates curl gnupg
 	sudo mkdir -p /etc/apt/keyrings
@@ -53,11 +53,11 @@ whiptail --title "Updating" --msgbox "Uprating repositories for nodejs and futur
 	sudo apt upgrade -y
 	VERSIONS=svxlink/src/versions
 
-	sudo echo -e $(date) " ${YELLOW}  *** commence build *** ${NORMAL}" >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) " ${YELLOW}  *** commence build *** ${NORMAL}" >> /var/log/'install.log'
 ############ BUILD ESSENTIALS ##########
 whiptail --title "Build Essentials" --msgbox "Adding all the packages necessary for Svxlink. Hit OK to continue" 8 78
 
-	sudo echo -e $(date) " ${YELLOW} Installing required software packages${NORMAL}" >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) " ${YELLOW} Installing required software packages${NORMAL}" >> /var/log/install.log'
 	sudo apt install build-essential g++ make cmake libsigc++-2.0-dev php8.2 nodejs libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y
 ########## REQUEST CALLSIGN ##########
 get_CallVar() {
@@ -80,10 +80,10 @@ done
 CALL=${user_input^^}
 # Use the non-empty name
 
-	echo $(date) ${GREEN} Creating Node " $CALL " *** ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e(date) ${GREEN} Creating Node " $CALL " *** ${NORMAL} >> /var/log/install.log'
 
 ########## GROUPS AND USERS ##########
-	sudo echo -e $(date) ${YELLOW} Creating Groups and Users ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) ${YELLOW} Creating Groups and Users ${NORMAL} >> /var/log/install.log'
 	sudo mkdir /etc/svxlink
     sudo groupadd svxlink
 	sudo useradd -g svxlink -d /etc/svxlink svxlink
@@ -92,29 +92,29 @@ CALL=${user_input^^}
 
 ########## DOWNLOADING SOURCE CODE ##########
 
-	sudo echo -e $(date) ${YELLOW} "########## Downloading SVXLink source code ##########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) ${YELLOW} "########## Downloading SVXLink source code ##########" ${NORMAL} >> /var/log/install.log'
 	cd
 	sudo git clone https://github.com/sm0svx/svxlink.git
 	sudo mkdir svxlink/src/build
 
 	
 	NEWVERSION=`sudo grep “SVXLINK=“ $VERSIONS | awk -F= '{print $2}'`
-	echo $(date) New Version: $NEWVERSION
+	sudo sh -c 'echo -e(date) New Version: $NEWVERSION >> /var/log/install.log'
 
 ########## COMPILING ##########
-	sudo echo -e $(date) ${YELLOW} "########## Compiling ##########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) ${YELLOW} "########## Compiling ##########" ${NORMAL} >> /var/log/install.log'
 	cd svxlink/src/build
 	sudo cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON  ..
 	sudo make
 	sudo make doc
-	echo $(date) ${GREEN}"########## Installing SVXLink ##########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e(date) ${GREEN}"########## Installing SVXLink ##########" ${NORMAL} >> /var/log/install.log'
 	sudo make install
 	cd /usr/share/svxlink/events.d
 	sudo mkdir local
 	sudo cp *.tcl ./local
 	sudo ldconfig
 ########### CONFIGURATION VOICES ##########
-	sudo echo -e $(date) ${GREEN} "########## Installing Voice Files ##########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) ${GREEN} "########## Installing Voice Files ##########" ${NORMAL} >> /var/log/install.log'
 	cd /usr/share/svxlink/sounds
 	sudo wget https://g4nab.co.uk/wp-content/uploads/2023/08/en_GB.tar_.gz
 	sudo tar -zxvf en_GB.tar_.gz
@@ -123,11 +123,11 @@ CALL=${user_input^^}
     sudo chmod 777 -R *
 
 ########### BACKUP CONFIGURATION ##########
-	echo $(date) backing up configuration to : $CONF.bak
+	sudo sh -c 'echo -e(date) backing up configuration to : $CONF.bak >> /var/log/install.log'
 	sudo cp -p $CONF $CONF.bak
 #
 	cd
-	sudo echo -e $(date) ${RED} "########## Downloading prepared configuration files from the scripts ##########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) ${RED} "########## Downloading prepared configuration files from the scripts ##########" ${NORMAL} >> /var/log/install.log'
 	sudo mkdir /home/pi/scripts
  	sudo cp -f svxlink_raspberry/10-uname /etc/update-motd.d/
 	sudo cp -f svxlink_raspberry/configs/svxlink.conf /etc/svxlink/
@@ -136,24 +136,24 @@ CALL=${user_input^^}
 	sudo cp -f svxlink_raspberry/resetlog.sh scripts/resetlog.sh
 	(sudo crontab -l 2>/dev/null; echo "59 23 * * * /home/pi/scripts/resetlog.sh ") | sudo crontab -
 
-	echo $(date) ${GREEN} Setting Callsign to "$CALL"${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e(date) ${GREEN} Setting Callsign to "$CALL"${NORMAL} >> /var/log/install.log'
 	sudo sed -i "s/MYCALL/$CALL/g" $CONF
 	sudo sed -i "s/MYCALL/$CALL/g" /etc/svxlink/node_info.json
 
-	echo $(date) Setting Squelch Hangtime to 10
+	sudo sh -c 'echo -e(date) Setting Squelch Hangtime to 10 >> /var/log/install.log'
 	sudo sed -i "s/SQL_HANGTIME=2000/SQL_HANGTIME=10/g" $CONF
 #	
-	echo $(date) Disabling audio distortion warning messages
+	sudo sh -c 'echo -e(date) Disabling audio distortion warning messages >> /var/log/install.log'
 	sudo sed -i "s/PEAK_METER=1/PEAK_METER=0/g" $CONF
 #
-	echo $(date) Updating SplashScreen on startup
+	sudo sh -c 'echo -e(date) Updating SplashScreen on startup >> /var/log/install.log'
 	sudo sed -i "s/MYCALL/$CALL/g" /etc/update-motd.d/10-uname
 	sudo chmod 0755 /etc/update-motd.d/10-uname
 #
-	echo $(date) Changing Log file
+	sudo sh -c 'echo -e(date) Changing Log file >> /var/log/install.log'
 	sudo sed -i "s/log\/svxlink/log\/svxlink.log/g" /etc/default/svxlink
 	########## INSTALLING DASHBOARD ##########
-	echo $(date) ${YELLOW} "######## Installing Dashboard ########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e(date) ${YELLOW} "######## Installing Dashboard ########" ${NORMAL} >> /var/log/install.log'
 	if [ $card=true ] ;
 	then
 	sudo sed -i "s/PTT_TYPE=GPIO/PTT_TYPE=Hidraw/g" $CONF
@@ -165,10 +165,10 @@ CALL=${user_input^^}
 	sudo sed -i "s/\#DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleEchoLink.conf
 	sudo sed -i "s/\#MUTE/MUTE/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
 	sudo sed -i "s/\#DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf	
- 	echo ${RED}Changing ModuleMetar Link${NORMAL} >> /var/log/install.log
+ 	sudo sh -c 'echo -e{RED}Changing ModuleMetar Link${NORMAL} >> /var/log/install.log'
   	sudo sed -i "s%#LINK=data/observations/metar/stations%LINK=/cgi-bin/data/dataserver.php?requestType=retrieve&dataSource=metars&hoursBeforeNow=3&stationString=
 %g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-	echo $(date) ${RED} Authorise GPIO setup service (Unused) and svxlink service${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e(date) ${RED} Authorise GPIO setup service (Unused) and svxlink service${NORMAL} >> /var/log/install.log'
 	sudo systemctl enable svxlink_gpio_setup
 	sleep 10
 	sudo systemctl enable svxlink
@@ -178,8 +178,8 @@ CALL=${user_input^^}
 	sudo systemctl start svxlink.service
 
 
-sudo echo -e $(date) ${RED}Installation complete\n${NORMAL} >> /var/log/install.log
-sudo echo -e $(date) ${GREEN} Reboot - Redémarrer SVXLink\n\n\n${NORMAL} >> /var/log/install.log
+sudo sh -c 'echo -e $(date) ${RED}Installation complete\n${NORMAL} >> /var/log/install.log'
+sudo sh -c 'echo -e $(date) ${GREEN} Reboot - Redémarrer SVXLink\n\n\n${NORMAL} >> /var/log/install.log'
 echo
 sleep 10
 
--- install_copy.sh
+++ install_copy.sh
@@ -19,7 +19,7 @@ if [ "$username" != "pi" ]; then echo "Please the reform the SDcard as user pi" 
 else whiptail --title "User check" --msgbox "User is pi" 8 78;
 fi
 ########## LOGGING ##########
-echo "OS is $OS_name and User is $current_user" >> /var/log/install.log
+echo "OS is $OS_name and User is $current_user" >> /var/log/install.log'
 
 ########## Welcome Message ##########
 source "${BASH_SOURCE%/*}/functions/welcome.sh"
@@ -51,22 +51,22 @@ exit
 
 
 #	NEWVERSION= `sudo grep "SVXLINK=" $VERSIONS | awk -F= '{print $2}'
-	echo $(date) New Version: $NEWVERSION
+	sudo sh -c 'echo -e(date) New Version: $NEWVERSION >> /var/log/install.log'
 ########## INSTALLATION ##########	
 ########## COMPILING ##########
-	sudo echo -e $(date) ${YELLOW} "########## Compiling ##########" ${NORMAL} >> /var/log/svxlink.log
+	sudo sh -c 'echo -e $(date) ${YELLOW} "########## Compiling ##########" ${NORMAL} >> /var/log/install.log'
 	cd svxlink/src/build
 	sudo cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON  ..
 	sudo make
 	sudo make doc
-	echo $(date) ${GREEN}"########## Installing SVXLink ##########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e(date) ${GREEN}"########## Installing SVXLink ##########" ${NORMAL} >> /var/log/install.log'
 	sudo make install
 	cd /usr/share/svxlink/events.d
 	sudo mkdir local
 	sudo cp *.tcl ./local
 	sudo ldconfig
 ########### CONFIGURATION VOICES ##########
-	sudo echo -e $(date) ${GREEN} "########## Installing Voice Files ##########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) ${GREEN} "########## Installing Voice Files ##########" ${NORMAL} >> /var/log/install.log'
 	cd /usr/share/svxlink/sounds
 	sudo wget https://g4nab.co.uk/wp-content/uploads/2023/08/en_GB.tar_.gz
 	sudo tar -zxvf en_GB.tar_.gz
@@ -75,11 +75,11 @@ exit
     sudo chmod 777 -R *
 
 ########### BACKUP CONFIGURATION ##########
-	echo $(date) backing up configuration to : $CONF.bak
+	sudo sh -c 'echo -e(date) backing up configuration to : $CONF.bak >> /var/log/install.log'
 	sudo cp -p $CONF $CONF.bak
 #
 	cd
-	sudo echo -e $(date) ${RED} "########## Downloading prepared configuration files from the scripts ##########" ${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e $(date) ${RED} "########## Downloading prepared configuration files from the scripts ##########" ${NORMAL} >> /var/log/install.log'
 	sudo mkdir /home/pi/scripts
  	sudo cp -f svxlink_raspberry/10-uname /etc/update-motd.d/
 	sudo cp -f svxlink_raspberry/configs/svxlink.conf /etc/svxlink/
@@ -88,24 +88,24 @@ exit
 	sudo cp -f svxlink_raspberry/resetlog.sh scripts/resetlog.sh
 	(sudo crontab -l 2>/dev/null; echo "59 23 * * * /home/pi/scripts/resetlog.sh ") | sudo crontab -
 
-	echo $(date) ${GREEN} Setting Callsign to "$CALL"${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e(date) ${GREEN} Setting Callsign to "$CALL"${NORMAL} >> /var/log/install.log'
 	sudo sed -i "s/MYCALL/$CALL/g" $CONF
 	sudo sed -i "s/MYCALL/$CALL/g" /etc/svxlink/node_info.json
 
-	echo $(date) Setting Squelch Hangtime to 10
+	sudo sh -c 'echo -e(date) Setting Squelch Hangtime to 10 >> /var/log/install.log'
 	sudo sed -i "s/SQL_HANGTIME=2000/SQL_HANGTIME=10/g" $CONF
 #	
-	echo $(date) Disabling audio distortion warning messages
+	sudo sh -c 'echo -e(date) Disabling audio distortion warning messages >> /var/log/install.log'
 	sudo sed -i "s/PEAK_METER=1/PEAK_METER=0/g" $CONF
 #
-	echo $(date) Updating SplashScreen on startup
+	sudo sh -c 'echo -e(date) Updating SplashScreen on startup >> /var/log/install.log'
 	sudo sed -i "s/MYCALL/$CALL/g" /etc/update-motd.d/10-uname
 	sudo chmod 0755 /etc/update-motd.d/10-uname
 #
-	echo $(date) Changing Log file
+	sudo sh -c 'echo -e(date) Changing Log file >> /var/log/install.log'
 	sudo sed -i "s/log\/svxlink/log\/svxlink.log/g" /etc/default/svxlink
 	########## INSTALLING DASHBOARD ##########
-	echo $(date) ${YELLOW} "######## Installing Dashboard ########" ${NORMAL} >> /var/log/svxlink.log
+	sudo sh -c 'echo -e(date) ${YELLOW} "######## Installing Dashboard ########" ${NORMAL} >> /var/log/svxlink.log'
 	if [ $card=true ] ;
 	then
 	sudo sed -i "s/PTT_TYPE=GPIO/PTT_TYPE=Hidraw/g" $CONF
@@ -117,10 +117,10 @@ exit
 	sudo sed -i "s/\#DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleEchoLink.conf
 	sudo sed -i "s/\#MUTE/MUTE/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
 	sudo sed -i "s/\#DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf	
- 	echo ${RED}Changing ModuleMetar Link${NORMAL} >> /var/log/install.log
+ 	sudo sh -c 'echo -e{RED}Changing ModuleMetar Link${NORMAL} >> /var/log/install.log'
   	sudo sed -i "s%#LINK=data/observations/metar/stations%LINK=/cgi-bin/data/dataserver.php?requestType=retrieve&dataSource=metars&hoursBeforeNow=3&stationString=
 %g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-	echo $(date) ${RED} Authorise GPIO setup service (Unused) and svxlink service${NORMAL} >> /var/log/install.log
+	sudo sh -c 'echo -e(date) ${RED} Authorise GPIO setup service (Unused) and svxlink service${NORMAL} >> /var/log/install.log'
 	sudo systemctl enable svxlink_gpio_setup
 	sleep 10
 	sudo systemctl enable svxlink
@@ -130,8 +130,8 @@ exit
 	sudo systemctl start svxlink.service
 
 
-sudo echo -e $(date) ${RED}Installation complete\n${NORMAL} >> /var/log/install.log
-sudo echo -e $(date) ${GREEN} Reboot - Redémarrer SVXLink\n\n\n${NORMAL} >> /var/log/install.log
+sudo sh -c 'echo -e $(date) ${RED}Installation complete\n${NORMAL} >> /var/log/install.log'
+sudo sh -c 'echo -e $(date) ${GREEN} Reboot - Redémarrer SVXLink\n\n\n${NORMAL} >> /var/log/install.log'
 echo
 sleep 10
 
