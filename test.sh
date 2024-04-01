b3d4cd8a341ce4bdd62419f09979ec363fc5d090
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-01 16:43:34 +0100

    m

--- test.sh
+++ test.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-########## INIT ##########
-source ./functions/init.sh
-welcome_message
-install_dependencies
-#kill_sound_cards
-get_CallVar
-echo $CALL
-node_type
-echo $OPTION
-query_network
-source ./functions/language.sh
-which_language
-source  ./functions/init.sh
-menu_end
+echo $USER
+sudo echo $USER
+echo $whoami
+sudo echo $whoami
 exit
 #
