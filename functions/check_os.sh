936f13223fe956d7a8a0a95f8decbf79060b9af7
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-01 16:03:45 +0100

    m

--- functions/check_os.sh
+++ functions/check_os.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
-# Assign the codename from the output of lsb_release -c to a variable
-#function check_os {
-# 
-#OS_name=$(lsb_release -c | grep -oP '(?<=Codename:\s)\w+')#
 
-## Print the value of the variable
-#if [ $OS_name != 'bookworm' ]; then
-#whiptail --title "SVXLink" --msgbox "This script is only for the Bookworm Linux distribution.\n\n Shutdown and Change to OS Bookworm, with user pi." 8 78
-#fi
-## Assign the current user of the terminal to a variable
-#source ./check_user.sh
-#user=$(get_current_user)
-#current_user=$user#
-
-## Print the value of the variable
-#if [ $current_user != "pi" ]; then
-#whiptail --title "SVXLink" --msgbox "This script is only for the pi user. Shutdown and reload the OS with user pi" 8 78
-#fi
-# 
-#}
 function check_os {
     # Retrieve the OS codename
     OS_name=$(lsb_release -c | grep -oP '(?<=Codename:\s)\w+')
@@ -34,7 +15,7 @@ function check_os {
 
 
 # Source the file containing the functions
-source ./check_user.sh
+source "${BASH_SOURCE%/*}/functions/check_user.sh"
 
 # Call the usercheck function
 usercheck
