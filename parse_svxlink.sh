35fbb5034d562a444c9468b0f0671c40530561c6
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-03 12:41:37 +0100

    m

--- parse_svxlink.sh
+++ parse_svxlink.sh
@@ -7,12 +7,16 @@ while IFS= read -r line; do
     line=$(echo "$line" | tr -d '\r\n') # Remove carriage return and newline characters
     if [[ $line == \[*]* ]]; then
         category=${line:1:-1}
+        # Write header category without parameters
+        if [ "$category" == "Header" ]; then
+            echo "$category" >> "$output_file"
+        fi
     elif [[ $line == \#* ]]; then
         # Parameter heading
         parameter_heading=$(echo "$line" | cut -d '=' -f 1 | sed 's/# *//')
         parameter=$(echo "$line" | cut -d '=' -f 2-)
         echo "$category,$parameter_heading,$parameter" >> "$output_file"
-    else
+    elif [[ -n "$line" ]]; then
         # Regular parameter
         parameter_heading=$(echo "$line" | cut -d '=' -f 1)
         parameter=$(echo "$line" | cut -d '=' -f 2-)
