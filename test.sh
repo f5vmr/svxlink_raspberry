357d704aacb017cc12f592c9da432a4a18128e4e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-03 21:41:01 +0100

    m

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run aplay -l and capture the output
 
-whiptail --scrolltext --title "SVXLink Configuration"  --textbox "$(cat config.txt)" 8 78 
+whiptail  --title "SVXLink Configuration"  --textbox config.txt 10 100 --scrolltext
 whiptail --title "Sound Card" --msgbox "This script will determine the USB soundcard's location. It will then display the location in the terminal." 8 78
 
 output=$(aplay -l)
