8a29ff61d0565bd1893db82eaca30ad83e436319
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-03 12:46:15 +0100

    m

--- parse_svxlink.sh
+++ parse_svxlink.sh
@@ -3,23 +3,30 @@
 input_file="svxlink.conf"
 output_file="svxlink.db"
 
+header_written=false
+
 while IFS= read -r line; do
     line=$(echo "$line" | tr -d '\r\n') # Remove carriage return and newline characters
     if [[ $line == \[*]* ]]; then
         category=${line:1:-1}
-        # Write header category without parameters
-        if [ "$category" == "Header" ]; then
+        # Write header category without parameters if not already written
+        if [ "$category" == "Header" ] && [ "$header_written" = false ]; then
             echo "$category" >> "$output_file"
+            header_written=true
         fi
     elif [[ $line == \#* ]]; then
         # Parameter heading
         parameter_heading=$(echo "$line" | cut -d '=' -f 1 | sed 's/# *//')
         parameter=$(echo "$line" | cut -d '=' -f 2-)
-        echo "$category,$parameter_heading,$parameter" >> "$output_file"
+        if [ "$header_written" = true ]; then
+            echo "$category,$parameter_heading,$parameter" >> "$output_file"
+        fi
     elif [[ -n "$line" ]]; then
         # Regular parameter
         parameter_heading=$(echo "$line" | cut -d '=' -f 1)
         parameter=$(echo "$line" | cut -d '=' -f 2-)
-        echo "$category,$parameter_heading,$parameter" >> "$output_file"
+        if [ "$header_written" = true ]; then
+            echo "$category,$parameter_heading,$parameter" >> "$output_file"
+        fi
     fi
 done < "$input_file"
