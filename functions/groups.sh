ca7323623d20229c5249dc71ff3706b10270777c
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-03 16:47:43 +0100

    m

--- functions/groups.sh
+++ functions/groups.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 function make_groups {
-   echo "$(date)" "${YELLOW} Creating Groups and Users ${NORMAL}" | tee /var/log/install.log
+   echo -e "$(date)" "${YELLOW} Creating Groups and Users ${NORMAL}" | tee /var/log/install.log
 #	sudo mkdir /etc/svxlink
 #    sudo groupadd svxlink
 #	sudo useradd -g svxlink -d /etc/svxlink svxlink
--- install_main_fr.sh
+++ install_main_fr.sh
@@ -26,34 +26,34 @@ update
 source "${BASH_SOURCE%/*}/functions/callsign.sh"
 callsign
 ########## GROUPS AND USERS ##########
-	echo "$(date)" "${YELLOW} Creating Groups and Users ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${YELLOW} Creating Groups and Users ${NORMAL}" | tee /var/log/install.log
 source "${BASH_SOURCE%/*}/functions/groups.sh"
 make_groups
 
 ########## DOWNLOADING SOURCE CODE ##########
 source "${BASH_SOURCE%/*}/functions/source.sh"
-	echo "$(date)" "${YELLOW} "########## Téléchargements de codesource SVXLink ##########" ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${YELLOW} ########## Téléchargements de codesource SVXLink ########## ${NORMAL}" | tee /var/log/install.log
 	cd
 svxlink_source	
 
 	
 	NEWVERSION=`sudo grep “SVXLINK=“ $VERSIONS | awk -F= '{print $2}'`
-	echo "$(date)" New Version: $NEWVERSION | tee /var/log/install.log
+	echo -e "$(date)" New Version: $NEWVERSION | tee /var/log/install.log
 
 ########## COMPILING ##########
-	echo "$(date)" "${YELLOW} "########## Compilation ##########" ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${YELLOW} ########## Compilation ########## ${NORMAL}" | tee /var/log/install.log
 	cd svxlink/src/build
 	sudo cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON  ..
 	sudo make
 	sudo make doc
-	echo "$(date)" "${GREEN}"########## Installation SVXLink ##########" ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${GREEN} ########## Installation SVXLink ########## ${NORMAL}" | tee /var/log/install.log
 	sudo make install
 	cd /usr/share/svxlink/events.d
 	sudo mkdir local
 	sudo cp *.tcl ./local
 	sudo ldconfig
 ########### CONFIGURATION VOICES ##########
-	echo "$(date)" "${GREEN} "########## Installation de dossiers Voix ##########" ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${GREEN} ########## Installation de dossiers Voix ########## ${NORMAL}" | tee /var/log/install.log
 	cd /usr/share/svxlink/sounds
 	sudo wget https://g4nab.co.uk/wp-content/uploads/2023/08/fr_FR.tar_.gz
 	sudo tar -zxvf fr_FR.tar_.gz
@@ -62,12 +62,12 @@ svxlink_source
     sudo chmod 777 -R *
 
 ########### BACKUP CONFIGURATION ##########
-	echo "$(date)" "${GREEN} backing up configuration to : $CONF.bak ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${GREEN} backing up configuration to : $CONF.bak ${NORMAL}" | tee /var/log/install.log
 
 	sudo cp -p $CONF $CONF.bak
 #
 	cd
-	echo "$(date)" "${RED} "########## Downloading prepared configuration files from the scripts ##########" ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${RED} ########## Downloading prepared configuration files from the scripts ########## ${NORMAL}" | tee /var/log/install.log
 	sudo mkdir /home/pi/scripts
  	sudo cp -f svxlink_raspberry/10-uname /etc/update-motd.d/
 	sudo cp -f svxlink_raspberry/configs/svxlink.conf /etc/svxlink/
@@ -76,24 +76,24 @@ svxlink_source
 	sudo cp -f svxlink_raspberry/resetlog.sh scripts/resetlog.sh
 	(sudo crontab -l 2>/dev/null; echo "59 23 * * * /home/pi/scripts/resetlog.sh ") | sudo crontab -
 
-	echo "$(date)" "${GREEN} Setting Callsign to "$CALL"${NORMAL}" |tee /var/log/install.log
+	echo -e "$(date)" "${GREEN} Setting Callsign to "$CALL"${NORMAL}" |tee /var/log/install.log
 	sudo sed -i "s/MYCALL/$CALL/g" $CONF
 	sudo sed -i "s/MYCALL/$CALL/g" /etc/svxlink/node_info.json
 
-	echo "$(date)" "${YELLOW} Setting Squelch Hangtime to 10 ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${YELLOW} Setting Squelch Hangtime to 10 ${NORMAL}" | tee /var/log/install.log
 	sudo sed -i "s/SQL_HANGTIME=2000/SQL_HANGTIME=10/g" $CONF 
 #	
-	echo "$(date)" "${YELLOW} Disabling audio distortion warning messages ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${YELLOW} Disabling audio distortion warning messages ${NORMAL}" | tee /var/log/install.log
 	sudo sed -i "s/PEAK_METER=1/PEAK_METER=0/g" $CONF
 #
-	echo "$(date)" "${GREEN} Updating SplashScreen on startup ${NORMAL}"  | tee /var/log/install.log
+	echo -e "$(date)" "${GREEN} Updating SplashScreen on startup ${NORMAL}"  | tee /var/log/install.log
 	sudo sed -i "s/MYCALL/$CALL/g" /etc/update-motd.d/10-uname
 	sudo chmod 0755 /etc/update-motd.d/10-uname
 #
-	echo "$(date)" "${GREEN} Changing Logfile suffix ${NORMAL}"| tee /var/log/install.log 		
+	echo -e "$(date)" "${GREEN} Changing Logfile suffix ${NORMAL}"| tee /var/log/install.log 		
 	sudo sed -i "s/log\/svxlink/log\/svxlink.log/g" /etc/default/svxlink
 	########## INSTALLING DASHBOARD ##########
-	echo "$(date)" "${YELLOW} "######## Installing Dashboard ########" ${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${YELLOW} ######## Installing Dashboard ######## ${NORMAL}" | tee /var/log/install.log
 	if [ $card=true ] ;
 	then
 	sudo sed -i "s/PTT_TYPE=GPIO/PTT_TYPE=Hidraw/g" $CONF
@@ -108,7 +108,7 @@ svxlink_source
  	echo{RED}Changing ModuleMetar Link${NORMAL}" | tee /var/log/install.log
   	sudo sed -i "s%#LINK=data/observations/metar/stations%LINK=/cgi-bin/data/dataserver.php?requestType=retrieve&dataSource=metars&hoursBeforeNow=3&stationString=
 %g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-	echo "$(date)" "${RED} Authorise GPIO setup service - Unused and svxlink service${NORMAL}" | tee /var/log/install.log
+	echo -e "$(date)" "${RED} Authorise GPIO setup service - Unused and svxlink service${NORMAL}" | tee /var/log/install.log
 	sudo systemctl enable svxlink_gpio_setup
 	sleep 3
 	sudo systemctl enable svxlink
@@ -118,8 +118,8 @@ svxlink_source
 	sudo systemctl start svxlink.service
 
 
-echo "$(date)" "${RED}Installation complete\n${NORMAL}" |tee /var/log/install.log
-echo "$(date)" ""${GREEN} Reboot - Redémarrer SVXLink\n\n\n${NORMAL}"" |tee /var/log/install.log
+echo -e "$(date)" "${RED}Installation complete\n${NORMAL}" |tee /var/log/install.log
+echo -e "$(date)" ""${GREEN} Reboot - Redémarrer SVXLink\n\n\n${NORMAL}"" |tee /var/log/install.log
 echo
 sleep 3
 
