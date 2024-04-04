2a37b0c93af95373731ed58adbc46496bf8100ed
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 20:59:15 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -27,15 +27,31 @@ selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27
         "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
         selected=$(echo "$selected" | sed 's/"//g')
         selected=$(echo "$selected" | tr ' ' ',')
-    sed -i "s/airports=/#airports=/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-    echo "airports=$selected" >> /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-    specifyairport=$(whiptail --title "Metar Info" --radiolist "Please specify the airport ICAO code for a default airport: " ${#airports} 27 78
-    )
-    
-
-      
-      
-      #$selected 
+    sed -i "s/AIRPORTS/#AIRPORTS=/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
+    echo "AIRPORTS=$selected" >> /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
+    specific_airport=$(whiptail --title "Metar Info" --radiolist "Please specify the airport ICAO code for a default airport: " ${#airports} 27 78 20 \
+        "EGLL" "London Heathrow" OFF \
+        "EGKK" "London Gatwick" OFF \
+        "EGCC" "Manchester" OFF \
+        "EGBB" "Birmingham" OFF \
+        "EGSS" "London Stansted" OFF \
+        "EGPF" "Glasgow" OFF \
+        "EGPH" "Edinburgh" OFF \
+        "EGPD" "Aberdeen Dyce" OFF \
+        "EGPK" "Prestwick" OFF \
+        "EGHH" "Bournemouth" OFF \
+        "EGHI" "Southampton" OFF \
+        "EGNT" "Newcastle" OFF \
+        "EGNX" "East Midlands" OFF \
+        "EGGW" "London Luton" OFF \
+        "EGGD" "Bristol" OFF \
+        "EGCN" "Sheffield" OFF \
+        "EGNM" "Leeds Bradford" OFF \
+        "EGNS" "Ronaldsway" OFF \
+        "EGAA" "Belfast Aldergrove" OFF \
+        "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
+        specific_airport=$(echo "$specific_airport" | sed's/"//g')
+        echo "STARTDEFAULT=$specific_airport" >> /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
     else
     exit 1
     fi
