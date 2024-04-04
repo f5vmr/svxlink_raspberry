f55dd81332f7d2ef39b7520dce63e0308027bbd4
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 19:32:31 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -4,22 +4,22 @@
 
 # Define options for the checklist
 airports=("EGLL" "EGKK" "EGCC" "EGBB" "EGSS" "EGPF" "EGPH" "EGPD" "EGPK" "EGHH" "EGHI" "EGNT" "EGNX" "EGGW" "EGGD" "EGCN" "EGNM" "EGNS" "EGAA" "EGBD")
-   
-# Calculate the height of the checklist window
-height=$(( ${#airports[@]} + 1)) # Add 1 to ensure height is at least 1
-width=78
+
+# Calculate the height and width of the checklist window
+height=$(( ${#airports[@]} + 2)) # Add 2 to account for title and button row
+width=80
 
 # Ask the user if they wish to configure the module
 if whiptail --title "Metar Info" --yesno "Do you wish to configure this module?" 10 60; then
-    # Construct the options list for whiptail
+    # Construct the options list for whiptail with manual multicolumn formatting
     options=()
     for airport in "${airports[@]}"; do
-        options+=("$airport" "$airport" OFF)
+        options+=("$airport" "$airport")
     done
 
     # Show the checklist with custom formatting
-    selected=$(whiptail --title "Metar Info" --checklist \
-        "Choose which Airports:" $height $width $((height - 1)) \
+    selected=$(whiptail --title "Metar Info" --separate-output --checklist \
+        "Choose which Airports:" $height $width $((height - 5)) \
         "${options[@]}" 3>&1 1>&2 2>&3)
 
     # Display selected options
@@ -30,4 +30,5 @@ if whiptail --title "Metar Info" --yesno "Do you wish to configure this module?"
 else
     exit 1
 fi
+
 
