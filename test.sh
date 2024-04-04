63c618c21355941bf9c380851fecbe63a30fdc2d
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 18:40:16 +0100

    m

--- test.sh
+++ test.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-# RuModuleMetarInfo
-mminfo=$(whiptail --title "ModuleMetarInfo" --yesno "Do you want to configure Airport Weather?" 8 78)
-    if[ $mminfo = "yes" ]
+#!/bin/bash
 
+# Define options for the checklist
+airports=(EGLL,EGKK,EGCC,EGBB,EGSS,EGPF,EGPH,EGPD,EGPK,EGHH,EGHI,EGNT,EGNX,EGGW,EGGD,EGCN,EGNM,EGNS,EGAA,EGBD)
+  
 
-else
-    #moving on
-fi
+# Calculate the height and width of the checklist window
+height=$(( ${#airports[@]} / 2 )) # Divide by 2 to account for descriptions
+width=60
 
-#
+# Show the checklist with custom formatting
+selected=$(whiptail --title "Airports" --checklist \
+    "Choose options:" $height $width $((height - 1)) \
+    "${options[@]}" 3>&1 1>&2 2>&3)
+
+# Display selected options
+echo "Airports selected:"
+for choice in $selected; do
+    echo "- $choice"
+done
