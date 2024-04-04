fa21cf2b93459c85d272ac6e3d9fd2ff6f50b621
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 19:43:00 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -5,27 +5,10 @@
 whiptail --title "Metar Info" --yesno "Do you wish to configure this module?" 10 60
     if [ $? -eq 0 ]; then
 selected=$(whiptail --title "Metar Info" --checklist \
-    "choose which Airports:" 10 78  \
-        1 "EGLL" \
-        2 "EGKK" \
-        3 "EGCC" \
-        4 "EGBB" \
-        5 "EGSS" \
-        6 "EGPF" \
-        7 "EGPH" \
-        8 "EGPD" \
-        9 "EGPK" \
-        10 "EGHH" \
-        11 "EGHI" \
-        12 "EGNT" \
-        13 "EGNX" \
-        14 "EGGW" \
-        15 "EGGD" \
-        16 "EGCN" \
-        17 "EGNM" \
-        18 "EGNS" \
-        19 "EGAA" \
-        20 "EGBD"  3>&1 1>&2 2>&3)
+    "choose which Airports:" $airports[@] 10 78  \
+    3>&1 1>&2 2>&3)
+          
+          
 
     
 
