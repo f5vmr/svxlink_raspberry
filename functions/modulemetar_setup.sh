6043ac5b8cc7d76c1f86b469a13c281b48428677
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 19:18:38 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -6,9 +6,14 @@ height=$(( ${#airports[@]} / 2))
 width=78
 whiptail --title "Metar Info" --yesno "Do you wish to configure this module?" 10 60
     if [ $? -eq 0 ]; then
-    selected=$(whiptail --title "Metar Info" --checklist \
+selected=$(whiptail --title "Metar Info" --checklist \
     "choose which Airports:" $height $width $((height - 1)) \
     "${airports[@]}" 3>&1 1>&2 2>&3
+
+    echo "Airports selected:"
+    for choice in $selected; do
+    echo "- $choice"
+    done
 else
     exit 1
     fi
