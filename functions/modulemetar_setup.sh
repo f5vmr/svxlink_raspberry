bfbf6796336a498033cd2ed88eda6bca17e0903a
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 19:14:25 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -2,7 +2,7 @@
 ##### Metar Info #####
  airports=("EGLL" "EGKK" "EGCC" "EGBB" "EGSS" "EGPF" "EGPH" "EGPD" "EGPK" "EGHH" "EGHI" "EGNT" "EGNX" "EGGW" "EGGD" "EGCN" "EGNM" "EGNS" "EGAA" "EGBD")
    
-height=$(( ${airports[@]} / 2))
+height=$(( ${#airports[@]} / 2))
 width=78
 whiptail --title "Metar Info" --yesno "Do you wish to configure this module?" 10 60
     if [ $? -eq 0 ]; then
