7dcbdbe61f26eee3b5333525e54026bbd2116004
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 18:27:51 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -7,26 +7,26 @@ airports=(EGLL,EGKK,EGCC,EGBB,EGSS,EGPF,EGPH,EGPD,EGPK,EGHH,EGHI,EGNT,EGNX,EGGW,
 whiptail --title "Metar Info" --yesno "Do you wish to configure this module?" 10 60
     if [ $? -eq 0 ]; then
     whiptail --title "Metar Info" --checklist $airports 27 78 4 \
-    "EGLL" "EGLL" OFF \
-    "EGKK" "EGKK" OFF \
-    "EGCC" "EGCC" OFF \
-    "EGBB" "EGBB" OFF \
-    "EGSS" "EGSS" OFF \
-    "EGPF" "EGPF" OFF \
-    "EGPH" "EGPH" OFF \
-    "EGPD" "EGPD" OFF \
-    "EGPK" "EGPK" OFF \
-    "EGHH" "EGHH" OFF \
-    "EGHI" "EGHI" OFF \
-    "EGNT" "EGNT" OFF \
-    "EGNX" "EGNX" OFF \
-    "EGGW" "EGGW" OFF \
-    "EGGD" "EGGD" OFF \
-    "EGCN" "EGCN" OFF \
-    "EGNM" "EGNM" OFF \
-    "EGNS" "EGNS" OFF \
-    "EGAA" "EGAA" OFF \
-    "EGBD" "EGBD" OFF 3>&1 1>&2 2>&3
+    "EGLL" "London Heathrow" OFF \
+    "EGKK" "London Gatwick" OFF \
+    "EGCC" "Manchester" OFF \
+    "EGBB" "Birmingham" OFF \
+    "EGSS" "London Stansted" OFF \
+    "EGPF" "Glasgow" OFF \
+    "EGPH" "Edinburgh" OFF \
+    "EGPD" "Aberdeen Dyce" OFF \
+    "EGPK" "Glasgow Prestwick" OFF \
+    "EGHH" "Bournemouth" OFF \
+    "EGHI" "Southampton" OFF \
+    "EGNT" "Newcastle" OFF \
+    "EGNX" "East Midlands" OFF \
+    "EGGW" "London Luton" OFF \
+    "EGGD" "Bristol" OFF \
+    "EGCN" "Sheffield" OFF \
+    "EGNM" "Leeds Bradford" OFF \
+    "EGNS" "Ronaldsway" OFF \
+    "EGAA" "Belfast Aldergrove" OFF \
+    "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3
 else
     exit 1
     fi
