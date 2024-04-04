76d79fc00de405cdc25eaa8fb76f004bd5882dab
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 19:01:31 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 ##### Metar Info #####
-airports=(EGLL,EGKK,EGCC,EGBB,EGSS,EGPF,EGPH,EGPD,EGPK,EGHH,EGHI,EGNT,EGNX,EGGW,EGGD,EGCN,EGNM,EGNS,EGAA,EGBD)
-    
+ airports=("EGLL" "EGKK" "EGCC" "EGBB" "EGSS" "EGPF" "EGPH" "EGPD" "EGPK" "EGHH" "EGHI" "EGNT" "EGNX" "EGGW" "EGGD" "EGCN" "EGNM" "EGNS" "EGAA" "EGBD")
+   
 
 
 whiptail --title "Metar Info" --yesno "Do you wish to configure this module?" 10 60
     if [ $? -eq 0 ]; then
     whiptail --title "Metar Info" --checklist $airports 25 78 25 \
-    "EGLL" "London Heathrow" OFF \
-    "EGKK" "London Gatwick" OFF \
-    "EGCC" "Manchester" OFF \
-    "EGBB" "Birmingham" OFF \
-    "EGSS" "London Stansted" OFF \
-    "EGPF" "Glasgow" OFF \
-    "EGPH" "Edinburgh" OFF \
-    "EGPD" "Aberdeen Dyce" OFF \
-    "EGPK" "Glasgow Prestwick" OFF \
-    "EGHH" "Bournemouth" OFF \
-    "EGHI" "Southampton" OFF \
-    "EGNT" "Newcastle" OFF \
-    "EGNX" "East Midlands" OFF \
-    "EGGW" "London Luton" OFF \
-    "EGGD" "Bristol" OFF \
-    "EGCN" "Sheffield" OFF \
-    "EGNM" "Leeds Bradford" OFF \
-    "EGNS" "Ronaldsway" OFF \
-    "EGAA" "Belfast Aldergrove" OFF \
-    "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3
+    
+    3>&1 1>&2 2>&3
 else
     exit 1
     fi
