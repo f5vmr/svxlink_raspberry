a62df3b500f61010fab0da0354ee0fd623c48d4e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-04 09:31:51 +0100

    m

--- test_bash.sh
+++ test_bash.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-echo "${BASH_SOURCE%/*}"
-exit
+
+# List of packages to install
+PACKAGES=("app1" "app2" "app3" "app4" "app5")
+
+# Total number of packages
+TOTAL=${#PACKAGES[@]}
+
+# Counter for progress
+PROGRESS=0
+
+# Function to update progress
+update_progress() {
+    PROGRESS=$((PROGRESS + 1))
+    echo "$(($PROGRESS * 100 / $TOTAL))"
+}
+
+# Loop through packages and install them
+for package in "${PACKAGES[@]}"; do
+    sudo apt install -y "$package" > /dev/null 2>&1
+    update_progress | whiptail --backtitle "Installing" --title "Installation Progress" --gauge "Installing $package" 6 60 0
+done
+
+# All packages installed
+whiptail --backtitle "Installing" --title "Installation Progress" --msgbox "All packages installed successfully!" 6 60
