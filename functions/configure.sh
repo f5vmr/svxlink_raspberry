8a09a209d04d81952a541f6bfae99f5b9f99d02d
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-05 15:12:55 +0100

    m

--- functions/configure.sh
+++ functions/configure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 svxlink="$CONF"svxlink.conf
 file=config.txt
-whiptail --title "SVXLink" --msgbox "$(cat $file
-)" 20 87 "OK" 2>&1 >/dev/null
+whiptail --title "SVXLink" --msgbox "$(cat $file)" 20 87 "OK" 2>&1 >/dev/null
 
 
