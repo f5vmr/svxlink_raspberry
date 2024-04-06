cebf7e85c2446b0950c3a879fe74b617743e4805
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-06 11:21:57 +0100

    m

--- test.sh
+++ test.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-
-## Define options for the checklist
-airports=("EGLL" "EGKK" "EGCC" "EGBB" "EGSS" "EGPF" "EGPH" "EGPD" "EGPK" "EGHH" "EGHI" "EGNT" "EGNX" "EGGW" "EGGD" "EGCN" "EGNM" "EGNS" "EGAA" "EGBD")
-  
-
-## Calculate the height and width of the checklist window
-height=$(( ${#airports[@]} / 2 )) ## Divide by 2 to account for descriptions
-width=60
-
-## Show the checklist with custom formatting
-selected=$(whiptail --title "Metar Info" --checklist \
-    "Choose the airports:" $height $width $((height - 1)) \
-    "${airports[@]}" 3>&1 1>&2 2>&3)
-
-## Display selected options
-echo "Airports selected:"
-for choice in $selected; do
-    echo "- $choice"
-done
+echo aplay -l 
+sudo chmod 777 /etc/modules
+ echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
+ sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
+ sudo sed -i "s/dtoverlay=vc4-kms-v3d/dtoverlay=vc4-kms-v3d,noaudio/g" /boot/firmware/config.txt
+ sudo cp /home/pi/svxlink_raspberry/configs/asound.conf /etc/modprobe.d/asound.conf
+ echo snd-aloop > /etc/modules
+ sudo cp /home/pi/svxlink_raspberry/configs/loopback.conf /etc/asound.conf
+ sudo /sbin/alsactl nrestore
