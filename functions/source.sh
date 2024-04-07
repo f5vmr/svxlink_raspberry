663d2049085d3d6338123cbdd129fa0eafd9c39e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-07 11:13:46 +0100

    m

--- functions/source.sh
+++ functions/source.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 function svxlink_source {
     cd /home/pi
+    echo -e "$(date)" "${YELLOW} Svxlink_source is SvxLink from SM0SVX ${NORMAL}" >> /var/log/install.log
     sudo git clone https://github.com/sm0svx/svxlink.git
 	sudo mkdir svxlink/src/build
 }
