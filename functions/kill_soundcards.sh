06684689d487987ce9b51afb3e148156b4d3cdf6
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-07 09:46:04 +0100

    m

--- functions/kill_soundcards.sh
+++ functions/kill_soundcards.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #### Kill OnBoard Soundcards ####
+function kill_soundcards {
+    sudo mkdir /etc/alsa
+    sudo touch /etc/alsa/state-daemon.conf
+    sudo systemctl restart alsa-state.service
     sudo touch /var/log/install.log
     sudo chmod 777 /var/log/install.log
     sudo setfacl -R -m u:$username:rwx /var/log/install.log
     sudo cp svxlink_install/install.sh ../
-    echo -e "$(date)" "${YELLOW} #### On Board Sound Cards Removed #### ${NORMAL}" | tee -a  /var/log/install.log
+    echo -e "$(date)" "${YELLOW} #### On Board Soundcards to be removed #### ${NORMAL}" | tee -a  /var/log/install.log
     sudo chmod 777 /etc/modules
     echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
     sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
@@ -12,6 +16,5 @@
     sudo cp /home/pi/svxlink_raspberry/configs/asound.conf /etc/modprobe.d/asound.conf
     echo snd-aloop > /etc/modules
     sudo cp /home/pi/svxlink_raspberry/configs/loopback.conf /etc/asound.conf
-    sudo /sbin/alsactl force-reload
-    echo -e "$(date)" "${YELLOW} #### Reboot - login and run ./install.sh #### ${NORMAL}" | tee -a  /var/log/install.log
-    sudo shutdown -r now
+    sudo /sbin/alsactl nrestore
+}
