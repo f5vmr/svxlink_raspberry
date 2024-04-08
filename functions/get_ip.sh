bba742ceacd289f4948f326a11065f815b8aee09
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-08 20:02:53 +0100

    m

--- functions/get_ip.sh
+++ functions/get_ip.sh
@@ -12,6 +12,6 @@ device=$(ip -4 route get 1 | grep -oP 'dev \K\S+') >> /var/log/install.log
 ## Update /etc/dhcpcd.conf with fixed IP addresses
 #*sudo sed -i "/interface $device/,+2 s/^static .*$/static ip_address=$ip_address\/24/" /etc/dhcpcd.conf
 
-echo -e "$(date)" "${GREEN}Fixed IP addresses updated in /etc/dhcpcd.conf:${NORMAL}" >> /var/log/install.log
+echo -e "$(date)" "${GREEN}Fixed IP address $ip_address with $device updated in /etc/dhcpcd.conf:${NORMAL}" >> /var/log/install.log
 
 }
