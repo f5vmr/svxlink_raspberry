a729bfa3fb8d96bf6eb1711b42131740ce7fff54
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-08 12:01:58 +0100

    m

--- preinstall.sh
+++ preinstall.sh
@@ -9,7 +9,8 @@
     sudo setfacl -R -m u:pi:rwx /var/log/install.log
     
     sudo chmod 777 /etc/modules
-    sudo echo blacklist snd_bcm2835 > /etc/modprobe.d/raspi-blacklist.conf
+    sudo sh -c 'echo "blacklist snd_bcm2835" > /etc/modprobe.d/raspi-blacklist.conf'
+
     sudo sed -i "s/options snd-usb/#options snd-usb/g" /lib/modprobe.d/aliases.conf
     sudo sed -i "s/dtoverlay=vc4-kms-v3d/dtoverlay=vc4-kms-v3d,noaudio/g" /boot/firmware/config.txt
     sudo cp /home/pi/svxlink_raspberry/configs/asound.conf /etc/modprobe.d/asound.conf
