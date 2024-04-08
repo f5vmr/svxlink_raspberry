b31a747c80edf817db9eadc9e26747f7433b4aab
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-08 20:01:50 +0100

    m

--- functions/get_ip.sh
+++ functions/get_ip.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-## Function to get IP address of an interface
-get_ip_address() {
-    local interface="$1"
-    ip addr show dev "$interface" | awk '$1 == "inet" { print $2 }' | cut -d'/' -f1
-}
 function ipaddress {
 ## Get current IP addresses
-eth_ip=$(get_ip_address "eth0")
-wifi_ip=$(get_ip_address "wlan0")
 
-## Check if IP addresses are empty
-if [ -z "$eth_ip" ] || [ -z "$wifi_ip" ]; then
-    echo "Error: Unable to determine IP addresses for Ethernet or Wi-Fi."
-    exit 1
-fi
+
+# Get the IPv4 address and device (wlan0 or eth0)
+ip_address=$(ip -4 route get 1 | grep -oP 'src \K\S+') >> /var/log/install.log
+device=$(ip -4 route get 1 | grep -oP 'dev \K\S+') >> /var/log/install.log
+
 
 ## Update /etc/dhcpcd.conf with fixed IP addresses
-#*sudo sed -i "/interface eth0/,+2 s/^static .*$/static ip_address=$eth_ip\/24/" /etc/dhcpcd.conf
-#*sudo sed -i "/interface wlan0/,+2 s/^static .*$/static ip_address=$wifi_ip\/24/" /etc/dhcpcd.conf
+#*sudo sed -i "/interface $device/,+2 s/^static .*$/static ip_address=$ip_address\/24/" /etc/dhcpcd.conf
 
-echo "Fixed IP addresses updated in /etc/dhcpcd.conf:"
-echo "Ethernet IP: $eth_ip"
-echo "Wi-Fi IP: $wifi_ip"
+echo -e "$(date)" "${GREEN}Fixed IP addresses updated in /etc/dhcpcd.conf:${NORMAL}" >> /var/log/install.log
+
 }
