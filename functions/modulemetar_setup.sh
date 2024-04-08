d50ee31bdee265319f6f1d8dbbf112cb39eb9b10
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-08 21:05:00 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -50,7 +50,7 @@ selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27
         "EGNS" "Ronaldsway" OFF \
         "EGAA" "Belfast Aldergrove" OFF \
         "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
-        specific_airport=$(echo "$specific_airport" | sed's/"//g')
+        #* specific_airport=$(echo "$specific_airport" | sed 's/"//g')
         #* echo "STARTDEFAULT=$specific_airport" >> /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
     #*    sed -i "s/DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf    
     else
