af3ded5cef2d122301f0ff14d2d61d8d0d620506
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-09 09:28:29 +0100

    m

--- functions/modulemetar_setup.sh
+++ functions/modulemetar_setup.sh
@@ -3,9 +3,9 @@
 function modulemetar {
  airports=("EGLL" "EGKK" "EGCC" "EGBB" "EGSS" "EGPF" "EGPH" "EGPD" "EGPK" "EGHH" "EGHI" "EGNT" "EGNX" "EGGW" "EGGD" "EGCN" "EGNM" "EGNS" "EGAA" "EGBD")
  
-whiptail --title "Metar Info" --yesno "Do you wish to configure this module?" 8 78 3>&1 1>&2 2>&3
-    if [ $? -eq 0 ]; then
-selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27 78 20 \
+metaryesno=(whiptail --title "Metar Info" --yesno "Do you wish to configure this module?" 8 78 3>&1 1>&2 2>&3)
+    if [ metaryesno == "yes" ]; then
+    selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27 78 20 \
         "EGLL" "London Heathrow" OFF \
         "EGKK" "London Gatwick" OFF \
         "EGCC" "Manchester" OFF \
@@ -30,7 +30,8 @@ selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27
         selected=$(echo "$selected" | tr ' ' ',')
     #* sed -i "s/AIRPORTS/#AIRPORTS=/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
     #* sudo sh -c 'echo "AIRPORTS=$selected"' >> /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-    specific_airport=$(whiptail --title "Metar Info" --radiolist "Please specify the airport ICAO code for a default airport: " ${#airports} 27 78 20 \
+        sleep 1
+        specific_airport=$(whiptail --title "Metar Info" --radiolist "Please specify the airport ICAO code for a default airport: " ${#airports} 27 78 20 \
         "EGLL" "London Heathrow" OFF \
         "EGKK" "London Gatwick" OFF \
         "EGCC" "Manchester" OFF \
@@ -51,9 +52,11 @@ selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27
         "EGNS" "Ronaldsway" OFF \
         "EGAA" "Belfast Aldergrove" OFF \
         "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
-        #* specific_airport=$(echo "$specific_airport" | sed 's/"//g')
+        specific_airport=$(echo "$specific_airport" | sed 's/"//g')
         #* echo "STARTDEFAULT=$specific_airport" >> /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-    #*    sed -i "s/DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf    
+        #* sed -i "s/DEFAULT_LANG=en_US/DEFAULT_LANG=en_GB/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf    
+        echo -e "$(date)" "${GREEN} $selected Airports included with default Airport $specific_airport ${NORMAL}"
+    fi
     else
     exit 1
     fi
