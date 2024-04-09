8eb0ee163181e88a0422540adb3706d928b72163
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-09 10:46:41 +0100

    m

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ check_os
 #### CHECK USER ####
 source "${BASH_SOURCE%/*}/functions/check_user.sh"
 usercheck
-echo -e "${GREEN} OS = $OS_name and Current user is $logname${NORMAL}" | tee -a  /var/log/install.log
+echo -e "${GREEN} OS = $operating_system and Current user is $logname${NORMAL}" | tee -a  /var/log/install.log
 #### SuperUser Install ####
 sudo ./svxlink_raspberry/install_main.sh
 
