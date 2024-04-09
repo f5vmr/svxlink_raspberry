227d2f0e43f42dd077f83845cbf1ba6a35838524
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-09 09:21:59 +0100

    m

--- functions/node_setup.sh
+++ functions/node_setup.sh
@@ -68,11 +68,13 @@ if [$HID == "false"] && [$GPIOD == "true"] && [$card == "false"]
             #*     sed -i's/#PTT_TYPE=Hidraw/PTT_TYPE=GPIOD/g' /etc/svxlink/svxlink.conf
             #*     sed -i's/#PTT_GPIOD_CHIP/PTT_GPIOD_CHIP/g' /etc/svxlink/svxlink.conf
             #*     sed -i's/#PTT_GPIOD_LINE=!24/PTT_GPIOD_LINE=$ptt_pin/g' /etc/svxlink/svxlink.conf
+            fi
             elif [$ptt_direction == "Low"] then
             echo
             #*    sed -i's/#PTT_TYPE=Hidraw/PTT_TYPE=GPIOD/g' /etc/svxlink/svxlink.conf
             #*    sed -i's/#PTT_GPIOD_CHIP/PTT_GPIOD_CHIP/g' /etc/svxlink/svxlink.conf
             #*    sed -i's/#PTT_GPIOD_LINE=!24/PTT_GPIOD_LINE=!$ptt_pin/g' /etc/svxlink/svxlink.conf
+            fi
             else 
             echo
             #no action
