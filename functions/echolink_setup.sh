f7a852470fea34952b91fbd8e2633008bad2f881
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-09 13:09:34 +0100

    m

--- functions/echolink_setup.sh
+++ functions/echolink_setup.sh
@@ -12,7 +12,7 @@ then
         echosysop=$(whiptail --title "System Operator Name?" --inputbox "Enter your SYSOP Name" 8 60 3>&1 1>&2 2>&3)
         echosysop=${echosysop^}
         echofreq=$(whiptail --title "Frequency" --inputbox "Enter your Output frequency in MHz eg 145.2375" 8 60 3>&1 1>&2 2>&3)
-        echolocation=$(whiptail --title "Location" --inputbox "Enter your location" 8 20 3>&1 1>&2 2>&3)
+        echolocation=$(whiptail --title "Location" --inputbox "Enter your location" 8 60 3>&1 1>&2 2>&3)
         echolocation=${echolocation^}
  #*   sed -i "s/CALLSIGN=MYCALL-L/CALLSIGN=$echocall/g" /etc/svxlink/svxlink.d/ModuleEchoLink.conf
  #*   sed -i "s/PASSWORD=MyPass/PASSWORD=$echopass/g" /etc/svxlink/svxlink.d/ModuleEchoLink.conf
