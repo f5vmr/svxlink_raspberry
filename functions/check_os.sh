aa78c109452088044f920db5fbc288a524ab3dd6
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-09 10:45:25 +0100

    m

--- functions/check_os.sh
+++ functions/check_os.sh
@@ -9,7 +9,7 @@ function check_os {
         whiptail --scrolltext --title "SVXLink" --msgbox "This script is only for the Bookworm Linux distribution. Shutdown and Change to OS Bookworm, with user pi." 8 78
         
         echo -e "${RED} Closing down the script - Wrong OS ${NORMAL}" >> /etc/var/install.log
-        sudo sh -c 'echo "sudo shutdown now"' >> /etc/var/install.log"
+        sudo sh -c 'echo "sudo shutdown now" >> /etc/var/install.log"'
         exit 1  ## Exit the script if the OS is not Bookworm
     fi
 }
