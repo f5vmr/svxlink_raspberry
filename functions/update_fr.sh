93a09f3af0840a72b234bc3b5e8f6ff144e40a17
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-09 10:39:14 +0100

    m

--- functions/update_fr.sh
+++ functions/update_fr.sh
@@ -13,7 +13,7 @@ whiptail --title "Mis à jour" --msgbox "Mis à jour les répositoires pour node
  #*	sudo mkdir -p /etc/apt/keyrings
  #*	curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
  #*	NODE_MAJOR=20
-	echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee -a  /etc/apt/sources.list.d/nodesource.list
+ #*	echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee -a  /etc/apt/sources.list.d/nodesource.list
 
  #*	sudo apt update
  #*	sudo apt upgrade -y
