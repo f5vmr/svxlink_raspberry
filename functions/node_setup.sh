ba7e81f96d820e59c83ddc84dcaaf80ab824b125
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-09 19:37:43 +0100

    m

--- functions/node_setup.sh
+++ functions/node_setup.sh
@@ -81,9 +81,9 @@ if [ "$HID" == "false" ] && [ "$GPIOD" == "true" ] && [ "$card" == "false" ]
             #*    sed -i's/#PTT_TYPE=Hidraw/PTT_TYPE=GPIOD/g' /etc/svxlink/svxlink.conf
             #*    sed -i's/#PTT_GPIOD_CHIP/PTT_GPIOD_CHIP/g' /etc/svxlink/svxlink.conf
             #*    sed -i's/#PTT_GPIOD_LINE=!24/PTT_GPIOD_LINE=!$ptt_pin/g' /etc/svxlink/svxlink.conf
-            
+            echo nothing
             else 
-            
+            echo
             #noaction
             fi   
         cos_direction=$(whiptail --title "COS" --radiolist "Please select COS direction" 8 78 2 \
