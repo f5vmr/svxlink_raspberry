83b8f092cbae5a346984611f73a77cc3742a2828
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-10 18:40:41 +0100

    m

--- functions/dash_install.sh
+++ functions/dash_install.sh
@@ -42,7 +42,7 @@ function install_dash {
  for item in "${LINES[@]}"; do
      echo "$item" | sudo tee -a /etc/sudoers.d/svxlink >/dev/null
  done
-
+sudo chmod 0440 /etc/sudoers.d/svxlink
 ## Check sudoers file syntax
  sudo visudo -c
 
