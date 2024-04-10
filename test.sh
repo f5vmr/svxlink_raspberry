97d10e6c9f90a32a2df2b4c3ca1c18b791b3936d
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-10 18:37:01 +0100

    m

--- test.sh
+++ test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
-# Get the IPv4 address and device (wlan0 or eth0)
-ip_address=$(ip -4 route get 1 | grep -oP 'src \K\S+')
-device=$(ip -4 route get 1 | grep -oP 'dev \K\S+')
-
-# Print the IPv4 address and device
-echo "Current IPv4 Address: $ip_address"
-echo "Device: $device"
-
-
+sudo touch /etc/sudoers.d/svxlink
+LINES=("svxlink ALL=NOPASSWD: /usr/sbin/service" "svxlink ALL=NOPASSWD: /bin/cp" "svxlink ALL=NOPASSWD: /bin/chown" "svxlink ALL=NOPASSWD: /bin/chmod" "svxlink ALL=NOPASSWD: /bin/systemctl" "svxlink ALL=NOPASSWD: /bin/reboot" "svxlink ALL=NOPASSWD: /bin/shutdown" )
  
+ ## Add each line to sudoers
+ for item in "${LINES[@]}"; do
+     echo "$item" | sudo tee -a /etc/sudoers.d/svxlink >/dev/null
+ done
+
+## Check sudoers file syntax
+ sudo visudo -c
+
+## Check if syntax check passed
+
+    echo "Lines added to sudoers successfully."
 
 
