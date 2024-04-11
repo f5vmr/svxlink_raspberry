8bfa7db5bb2089b4c58d57835a058dcb1b19adc9
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-11 18:40:39 +0100

    m

--- test_bash.sh
+++ test_bash.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-install_locale() {
-        locale=$1
-        if ! locale -a | grep -q "${locale}"; then
-            sudo sh -c "locale-gen ${locale}"
-        fi
-    }
-    
     # Install en_GB.UTF-8 locale if not available
-    install_locale "en_GB.UTF-8"
+    sudo sh -c "locale-gen en_GB.UTF-8"
     # Install fr_FR.UTF-8 locale if not available
-    install_locale "fr_FR.UTF-8"
+    sudo sh -c "locale-gen fr_FR.UTF-8"
     # Install en_US.UTF-8 locale if not available
-    install_locale "en_US.UTF-8"
+    sudo sh -c "locale-gen en_US.UTF-8"
     # Install es_ES.UTF-8 locale if not available
-    install_locale "es_ES.UTF-8"
+    sudo sh -c "locale-gen es_ES.UTF-8"
     # Set en_GB.UTF-8 as the default locale
     localectl set-locale LANG=en_GB.UTF-8
 
