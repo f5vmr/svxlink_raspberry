10f9390dae1c6f6f31a33115335e8c411b3f321b
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-11 16:34:00 +0100

    m

--- test.sh
+++ test.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-sudo touch /etc/sudoers.d/svxlink
-LINES=("svxlink ALL=NOPASSWD: /usr/sbin/service" "svxlink ALL=NOPASSWD: /bin/cp" "svxlink ALL=NOPASSWD: /bin/chown" "svxlink ALL=NOPASSWD: /bin/chmod" "svxlink ALL=NOPASSWD: /bin/systemctl" "svxlink ALL=NOPASSWD: /bin/reboot" "svxlink ALL=NOPASSWD: /bin/shutdown" )
- 
- ## Add each line to sudoers
- for item in "${LINES[@]}"; do
-     echo "$item" | sudo tee -a /etc/sudoers.d/svxlink >/dev/null
- done
-sudo chmod 0440 /etc/sudoers.d/svxlink
+LANG_OPTION=$(whiptail --title "Language Option" --menu "Select Default Language" 10 78 2 \
+        "1" "English - en_GB" \
+        "2" "French - fr_FR" 3>&1 1>&2 2>&3)
 
-## Check sudoers file syntax
- sudo visudo -c
-
-## Check if syntax check passed
-
-    echo "Lines added to sudoers successfully."
-
+    case ${LANG_OPTION} in
+        1)
+            LC_ALL="en_GB.UTF-8"
+            LANG="en_GB.UTF-8"
+            LANGUAGE="en_GB:en"
+            ;;
+        2)
+            LC_ALL="fr_FR.UTF-8"
+            LANG="fr_FR.UTF-8"
+            LANGUAGE="fr_FR:fr:en_GB:en"
+            ;;
+        *)
+            echo "ALERT" "${FUNCNAME}: unknown language option '${LANG_OPTION}'"
+            ;;
+    esac
+    LC_PAPER="en_GB.UTF-8"  # default paper locale
+    LESSCHARSET="utf-8"      # independent from locale
+    MM_CHARSET="utf-8"       # independent from locale
+    echo "locale settings" "${LANG}"
+    export LC_ALL LANG LANGUAGE LC_PAPER LESSCHARSET MM_CHARSET
 
