42cba05fc75acaa510254c68eace17f82f344ba0
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-11 17:39:58 +0100

    m

--- test.sh
+++ test.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-    install_locales() {
-    if ! locale -a | grep -q "en_GB.UTF-8"; then
-    sudo locale-gen en_GB.UTF-8
-    fi
-    
-    if ! locale -a | grep -q "fr_FR.UTF-8"; then
-    sudo locale-gen fr_FR.UTF-8
-    fi
-}
 
 # Install locales
-    sudo install_locales
+    
     LANG_OPTION=$(whiptail --title "Language Option" --menu "Select Default Language" 10 78 2 \
         "1" "English - en_GB" \
         "2" "French - fr_FR" 3>&1 1>&2 2>&3)
@@ -25,7 +16,7 @@
         2)
             LC_ALL="fr_FR.UTF-8"
             LANG="fr_FR.UTF-8"
-            LANGUAGE="fr_FR:fr:en_GB:en"
+            LANGUAGE="fr_FR:fr"
             ;;
         *)
             echo "ALERT" "${FUNCNAME}: unknown language option '${LANG_OPTION}'"
