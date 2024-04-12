4df9273ad3ff861204b9de2be24d8cba9429cdab
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-12 09:49:40 +0100

    m

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+# Function to install locales if not already available
 install_locale() {
     locale=$1.UTF-8
-    if [ ! -e "/usr/lib/locale/${locale}" ]; then
+    if ! locale -a | grep -q "^${locale}$"; then
         sudo locale-gen ${locale}
     fi
 }
@@ -11,3 +13,9 @@ install_locale "en_GB"
 install_locale "fr_FR"
 install_locale "en_US"
 install_locale "es_ES"
+
+# Set en_GB.UTF-8 as the default locale
+sudo localectl set-locale LANG=en_GB.UTF-8
+
+echo "Locale setup completed. Rebooting..."
+sudo reboot
