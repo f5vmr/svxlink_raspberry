01daffc3e8a0c981d8c458206776d6c92466505e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-12 11:24:57 +0100

    m

--- functions/dash_install_fr.sh
+++ functions/dash_install_fr.sh
@@ -39,8 +39,8 @@ function install_dash {
  sudo touch /etc/sudoers.d/svxlink
  
  ## Add each line to sudoers
- for item in "${LINES[@]}"; do
-     echo "$item" | sudo tee -a /etc/sudoers.d/svxlink >/dev/null
+ for line in "${LINES[@]}"; do
+     echo "$line" | sudo tee -a /etc/sudoers.d/svxlink >/dev/null
  done
 sudo chmod 0440 /etc/sudoers.d/svxlink
 ## Check sudoers file syntax
