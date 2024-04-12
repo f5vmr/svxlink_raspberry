83967fab03c1626fd8dabd40246a9bbc8a13587c
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-12 11:54:37 +0100

    m

--- functions/get_ip.sh
+++ functions/get_ip.sh
@@ -11,8 +11,8 @@ device=$(ip -4 route get 1 | grep -oP 'dev \K\S+')
 
 ## Update /etc/dhcpcd.conf with fixed IP addresses
 sudo touch /etc/dhcpcd.conf | sudo chmod 777 /etc/dhcpcd.conf
-sudo sh -c `echo "interface $device" | tee -a /etc/dhcpcd.conf`
-sudo sh -c `echo "static ip_address=$ip_address/24" | tee -a /etc/dhcpcd.conf`
+sudo sh -c "echo 'interface $device' | tee -a /etc/dhcpcd.conf > /dev/nul "
+sudo sh -c "echo 'static ip_address=$ip_address/24' | tee -a /etc/dhcpcd.conf > /dev/nul"
 
 echo -e "$(date)" "${GREEN} Fixed IP address $ip_address with $device updated in /etc/dhcpcd.conf:${NORMAL}" | tee -a /var/log/install.log
 
